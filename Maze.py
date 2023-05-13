0b24b67ad90702cea983348e0eb44060dcd7d80b
ikhlaas <21619513@student.curtin.edu.au>
2023-05-13 19:11:44 +0400

    eight commit, updating Maze.py in messages branch

--- Maze.py
+++ Maze.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
             elif grid[currentRow][currentCol] == END:
                 done = True
                 solved = True
-                print("MESSAGE 4") # Solved.
+                print("Solved") # Solved.
                 
             else:
                 pass # Do nothing
@@ -86,7 +86,7 @@ if __name__ == "__main__":
 
 
     if not solved:
-        print("MESSAGE 5") # Did not reach the end.
+        print("You have fail to escape, Future archeologogist gaze upon your remains in the bafflement") # Did not reach the end.
 
 
     Viewer.view(grid)
